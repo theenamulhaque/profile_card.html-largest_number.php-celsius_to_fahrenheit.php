3b320ab27ebce82fd13e081e70d3d3745f369f39
Md Enamul  Haque <enamulhaquecoder@gmail.com>
2024-07-05 18:18:29 +0600

    Module-5 assignment

---
+++ celsius_to_fahrenheit.php/celsius_to_fahrenheit.php
@@ -0,0 +1,6 @@
+<?php 
+
+$celsius = 32;
+$fahrenheit = ($celsius * 9/5) + 32;
+
+printf("Celsius Temperature convert to Fahrenheit is: %.2f", $fahrenheit);
---
+++ largest_number.php/largest_number.php
@@ -0,0 +1,16 @@
+<?php 
+
+$num1 = 4;
+$num2 = 5;
+$num3 = 6;
+
+if ($num1 > $num2 && $num1 > $num3) {
+      echo 'num1 is the Largest Number amoung 3';
+}
+elseif($num2 > $num3){
+      echo 'num2 is the Largest Number amoung 3';
+}
+else {
+      echo 'num3 is the Largest Number amoung 3';
+}
+
